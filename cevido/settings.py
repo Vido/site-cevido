342c69918c764676d85dc1f52595f32c7ff3ff73
Lucas Vido <vido@usp.br>
2014-07-25 15:52:52 -0300

    Deploy issued fix.

--- cevido/settings.py
+++ cevido/settings.py
@@ -28,9 +28,8 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'photologue',
     'south',
     'newsletter',
