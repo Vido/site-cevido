1f0ce2927e955c57fb086245c41c35b66b643306
Vido <vido@usp.br>
2014-12-26 21:48:23 -0200

    Some real world data

--- scripts/dev_properties_load.sh
+++ scripts/dev_properties_load.sh
@@ -6,5 +6,4 @@ cd $SITE_DIR
 rm db*
 python manage.py syncdb --noinput
 python manage.py migrate
-python manage.py loaddata ../scripts/dev_real_estate_property.json
-python manage.py loaddata ../scripts/dev_auth_users.json
+python manage.py loaddata ../scripts/fixtures/dev_initial_data.json
