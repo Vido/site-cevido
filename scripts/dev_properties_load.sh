10394e0bf6ff9f4b3ef829519274f59530c5a330
Vido <vido@usp.br>
2015-03-29 12:11:49 -0300

    New load_properties script

--- scripts/dev_properties_load.sh
+++ scripts/dev_properties_load.sh
@@ -3,8 +3,6 @@
 source envvars.config
 cd $SITE_DIR
 
-rm db*
-python manage.py syncdb --noinput
-python manage.py migrate
-# python manage.py loaddata ../scripts/fixtures/dev_initial_data.json
-# python manage.py loaddata real_estate/fixtures/real_estate_default_gallery.json
+python manage.py loaddata real_estate/fixtures/city_fixture.json
+python manage.py loaddata real_estate/fixtures/owner_fixture.json
+python manage.py loaddata real_estate/fixtures/property_fixture.json
